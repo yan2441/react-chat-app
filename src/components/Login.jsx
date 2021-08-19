b937e40ae542773dff8b411f2f2037a2db02a073
Yassine najem <yasen2441@gmail.com>
2021-08-19 19:53:01 +0100

    set up loging methods

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { GoogleOutlined, FacebookOutlined } from "@ant-design/icons"
+import "firebase/app"
 
+import { auth } from '../firebase';
+import firebase from 'firebase/app';
 
 const Login = () => {
   return (
@@ -8,11 +11,13 @@ const Login = () => {
       <div id="login-card">
         <h2>Welcome to chat</h2>
 
-        <div className="login-button google">
+        <div className="login-button google"
+          onClick={() => auth.signInWithRedirect(new firebase.auth.GoogleAuthProvider())}>
           <GoogleOutlined />Sign in with google
         </div>
         <br /><br />
-        <div className="login-button facebook">
+        <div className="login-button facebook"
+          onClick={() => auth.signInWithRedirect(new firebase.auth.FacebookAuthProvider())}>
           <FacebookOutlined />Sign in with Facebook
         </div>
       </div>
