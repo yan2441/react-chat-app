a02e1471fd4cc421e8c0dcfb3c5eec430257b08d
Yassine najem <yasen2441@gmail.com>
2021-08-19 19:55:28 +0100

    wrap app with context to handel Auth of user

---
+++ src/contexts/AuthContext.jsx
@@ -0,0 +1,30 @@
+import React, { useEffect, useState, useContext } from 'react'
+import { useHistory } from 'react-router'
+import { auth } from '../firebase'
+
+const AuthContext = React.createContext();
+
+export const useAuth = () => useContext(AuthContext)
+
+export const AuthProvider = ({ children }) => {
+  const [loading, setLoding] = useState(true);
+  const [user, setUser] = useState(true);
+  const history = useHistory();
+
+  useEffect(() => {
+    auth.onAuthStateChanged((user) => {
+      setUser(user)
+      setLoding(false)
+      history.push("/chats")
+    })
+  }, [user, history]);
+
+
+  const value = { user }
+
+  return (
+    <AuthContext.Provider value={value}>
+      {!loading && children}
+    </AuthContext.Provider>
+  )
+}
