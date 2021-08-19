2f37da7d3b9122b2aba8d8e70fb77e612f523665
Yassine najem <yasen2441@gmail.com>
2021-08-19 16:00:59 +0100

    make the login screen

--- src/components/App.js
+++ src/components/App.js
@@ -5,17 +5,17 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
 // import { AuthProvider } from "../contexts/AuthContext"
 
 // import Chats from "./Chats"
-// import Login from "./Login"
+import Login from "./Login"
 
 function App() {
   return (
     <div style={{ fontFamily: 'Avenir' }}>
       <Router>
         {/* <AuthProvider> */}
-          <Switch>
-            {/* <Route path="/chats" component={Chats} />
-            <Route path="/" component={Login} /> */}
-          </Switch>
+        <Switch>
+          {/* <Route path="/chats" component={Chats} /> */}
+          <Route path="/" component={Login} />
+        </Switch>
         {/* </AuthProvider> */}
       </Router>
     </div>
